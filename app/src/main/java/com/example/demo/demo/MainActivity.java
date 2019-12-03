29c0c5bbdaf312a0efe27de46f08f5cb2a083d15
ruanxuexiong <412877174@qq.com>
2019-12-03 21:49:17 +0800

    Conflicts: 	app/src/main/java/com/example/demo/demo/MainActivity.java

--- app/src/main/java/com/example/demo/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/demo/MainActivity.java
@@ -17,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //jjk
+        FloatingActionButton fabs = (FloatingActionButton) findViewById(R.id.fab);
+        fabs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
