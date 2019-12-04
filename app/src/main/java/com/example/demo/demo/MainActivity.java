40b92c16605e35cb949d2bd19e28425c66a825c5
ruanxuexiong <412877174@qq.com>
2019-12-04 13:42:47 +0800

    Merge branch 'master' of https://github.com/ruanxuexiong/Demo2 into master11
    # Conflicts:
    #	app/src/main/java/com/example/demo/demo/MainActivity.java

--- app/src/main/java/com/example/demo/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/demo/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //jjk得到jkh'h'h77j'j'j'j'j
-        FloatingActionButton fabp'p = (FloatingActionButton) findViewById(R.id.fab);
-        fabs.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabp = (FloatingActionButton) findViewById(R.id.fab);
+        fabp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
